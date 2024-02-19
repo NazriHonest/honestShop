2af974cb225333cf24ba5a59f7830f0f5908f9ea
NazriHonest <naazir21@gmail.com>
2024-02-19 16:38:26 +0000

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controller/userController.js';
+import { createUser, deleteUser, getUserById, getUsers, login, updateUser } from '../controller/userController.js';
 
 const router = express.Router();
 
 router.route('/').get(getUsers).post(createUser)
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser)
+router.route('/login').post(login)
 
 export default router
